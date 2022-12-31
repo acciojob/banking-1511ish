99745f70430622349fe8b9b3328b78c3e17edebd
Ishant Pal <mrishantsre1511ish@gmail.com>
2022-12-31 13:32:36 +0530

    banking

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -3,10 +3,12 @@ package com.driver;
 public class StudentAccount extends BankAccount{
 
     String  institutionName;
-
+    public String getInstitutionName(){
+        return this.institutionName;
+    }
     public StudentAccount(String name, double balance, String  institutionName) {
         //minimum balance is 0 by default
-
+        super(name,balance,0);
     }
 
 }
